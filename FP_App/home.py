28eaaec91b1d712f44084c26e53f3feebe594bf0
Yassu <yasmine.chakkour@gmail.com>
2025-03-13 13:32:26 +0400

    Update home.py

--- FP_App/home.py
+++ FP_App/home.py
@@ -32,6 +32,7 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
 
 # Text cleaning functions
 lemmatizer = WordNetLemmatizer()
