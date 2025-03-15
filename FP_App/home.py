4ca83e196a734ca20a84cc4b0b785eb6038ed19e
Yassu <yasmine.chakkour@gmail.com>
2025-03-15 18:17:00 +0400

    Update home.py

--- FP_App/home.py
+++ FP_App/home.py
@@ -94,8 +94,8 @@ with col2:
             border: 2px solid white !important;
             font-size: 16px !important;
             font-weight: bold !important;
-            padding: 8px 5px !important;
-            margin: 4px 0px !important;
+            padding: 8px 10px !important;
+            margin: 3px 0px !important;
         }
         """,
     ):
